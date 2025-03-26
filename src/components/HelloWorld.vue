949a171e8c06833565d3f5f7e5cccb03a64c77bc
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-03-25 23:21:49 -0600

    subi proyecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const bienvenida = ref("Bienvenidos a la seccion de condicionales")
+const nombre = ref("Ana Kimberly")
+const apellidos = ref ("Hernandez Perez")
+//defineProps({
+  //msg: String,
+//})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h2> {{ bienvenida }}</h2>
+</template>
+
+<style scoped>
+
+</style>
